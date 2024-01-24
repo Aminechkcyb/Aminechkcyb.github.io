31eb1361e9e90d338a5c2158c95657e5356a87bc
Aminechkcyb <amine.chikh@ynov.com>
2024-01-24 16:40:52 +0100

    update

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 var bouton = document.getElementById("monBouton");
-    var links = document.querySelectorAll('.navbar a');
+var links = document.querySelectorAll('.navbar a');
 
-    bouton.addEventListener("click", function() {
-        // Toggle le display des liens dans la navbar lorsque le bouton est cliqué
-        links.forEach(function(link) {
-            link.style.display = (link.style.display === "none" || link.style.display === "") ? "block" : "none";
-        });
-    });
-
-    // Ajouter un gestionnaire d'événements pour les liens dans la navbar
+function toggleNavbar() {
+    // Toggle le display des liens dans la navbar lorsque le bouton est cliqué
     links.forEach(function(link) {
-        link.addEventListener("click", function() {
-            // Fermer le header lorsqu'on clique sur un lien
-            links.forEach(function(link) {
-                link.style.display = "none";
-            });
-        });
+        link.style.display = (link.style.display === "none" || link.style.display === "") ? "block" : "none";
     });
+}
+
+// Ajouter un gestionnaire d'événements pour le clic sur le bouton
+bouton.addEventListener("click", function() {
+    toggleNavbar();
+});
+
+// Aucun besoin de gestionnaire d'événements pour les liens dans la navbar si vous ne voulez pas les cacher lorsqu'ils sont cliqués
